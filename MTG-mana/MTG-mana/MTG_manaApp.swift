5e7200085eac0b896b7c633756194ea7b7f3b4eb
Micael Aleksis Raste <118250773+My-kull@users.noreply.github.com>
2023-08-08 14:47:18 +0300

    Add files via upload

---
+++ MTG-mana/MTG-mana/MTG_manaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTG_manaApp.swift
+//  MTG-mana
+//
+//  Created by Micael Raste on 1.6.2023.
+//
+
+import SwiftUI
+
+@main
+struct MTG_manaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
