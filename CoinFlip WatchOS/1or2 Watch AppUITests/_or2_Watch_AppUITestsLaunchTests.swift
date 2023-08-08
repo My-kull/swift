f4e76cfb875efafb73c1d5085edad5ab58e0928e
Micael Aleksis Raste <118250773+My-kull@users.noreply.github.com>
2023-08-08 14:58:17 +0300

    Add files via upload

---
+++ CoinFlip WatchOS/1or2 Watch AppUITests/_or2_Watch_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _or2_Watch_AppUITestsLaunchTests.swift
+//  1or2 Watch AppUITests
+//
+//  Created by Micael Raste on 2.5.2023.
+//
+
+import XCTest
+
+final class _or2_Watch_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
